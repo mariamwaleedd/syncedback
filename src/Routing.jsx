1e204d9e9db771248429dd3b483b35e708367f4a
Mariam Waleed <mariamwaleed2005@gmail.com>
2026-04-04 12:43:09 +0200

    feat: added files & folders

---
+++ src/Routing.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+const Routing = () => {
+    return (
+        <>
+        
+        
+        </>
+      );
+}
+ 
+export default Routing;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Routing from './Routing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <Routing/>
   </React.StrictMode>
 );
 
