dbaed5e8ec484606c5afcf120673f1587d8f76ba
Mariam Waleed <mariamwaleed2005@gmail.com>
2026-04-08 14:58:24 +0200

    fix: blocked global selection around all text in code

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const savedTheme = localStorage.getItem('synced_theme') || 'dark';
 document.documentElement.setAttribute('data-theme', savedTheme);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
